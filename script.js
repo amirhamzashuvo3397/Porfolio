14f7f81b8c31f01c3f462efa1677c499c2090281
ahs3397 <134881670+amirhamzashuvo3397@users.noreply.github.com>
2023-09-18 22:35:39 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+$(document).ready(function() {
+
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1) {
+        $(".header-area").addClass("sticky");
+      } else {
+        $(".header-area").removeClass("sticky");
+      }
+  
+     
+      updateActiveSection();
+    });
+  
+    $(".header ul li a").click(function(e) {
+      e.preventDefault(); 
+  
+      var target = $(this).attr("href");
+  
+      if ($(target).hasClass("active-section")) {
+        return; 
+      }
+  
+      if (target === "#home") {
+        $("html, body").animate(
+          {
+            scrollTop: 0 
+          },
+          500
+        );
+      } else {
+        var offset = $(target).offset().top - 40; 
+  
+        $("html, body").animate(
+          {
+            scrollTop: offset
+          },
+          500
+        );
+      }
+  
+      $(".header ul li a").removeClass("active");
+      $(this).addClass("active");
+    });
+  
+
+    ScrollReveal({
+      distance: "100px",
+      duration: 2000,
+      delay: 200
+    });
+  
+    ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", {
+      origin: "left"
+    });
+
+    ScrollReveal().reveal(".project-title, .contact-title", {
+      origin: "top"
+    });
+    ScrollReveal().reveal(".projects, .contact", {
+      origin: "bottom"
+    });
+
+  form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then(response => {
+              msg.innerHTML = "Message sent successfully"
+              setTimeout(function () {
+                  msg.innerHTML = ""
+              }, 5000)
+              form.reset()
+          })
+          .catch(error => console.error('Error!', error.message))
+  })
+    
+  });
+  
+  function updateActiveSection() {
+    var scrollPosition = $(window).scrollTop();
+  
+    // Checking if scroll position is at the top of the page
+    if (scrollPosition === 0) {
+      $(".header ul li a").removeClass("active");
+      $(".header ul li a[href='#home']").addClass("active");
+      return;
+    }
+  
+  
+    $("section").each(function() {
+      var target = $(this).attr("id");
+      var offset = $(this).offset().top;
+      var height = $(this).outerHeight();
+  
+      if (
+        scrollPosition >= offset - 40 &&
+        scrollPosition < offset + height - 40
+      ) {
+        $(".header ul li a").removeClass("active");
+        $(".header ul li a[href='#" + target + "']").addClass("active");
+      }
+    });
+  }
+  
+
+ 
